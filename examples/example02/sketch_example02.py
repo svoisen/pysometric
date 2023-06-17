bd656cc82e7062f405b9bb237c92471e7960d7cb
Sean Voisen <svoisen@adobe.com>
2023-06-16 22:33:55 -0700

    Update examples

--- examples/example02/sketch_example02.py
+++ examples/example02/sketch_example02.py
@@ -1,22 +1,29 @@
-import vsketch
-import pysometric as psm
 import shapely
-import math
+import vsketch
+
+import pysometric as psm
 
 
-class Example03Sketch(vsketch.SketchClass):
+class Example02Sketch(vsketch.SketchClass):
     unit_size = vsketch.Param(1.0, unit="cm")
 
     def draw(self, vsk: vsketch.Vsketch) -> None:
         vsk.size("a4", landscape=False, center=False)
         frame = shapely.box(0, 0, vsk.width, vsk.height)
-        cube = psm.Box((0, 0, 0.5), left={"textures": [psm.HatchTexture(3.5)]}, right={"textures": [psm.FillTexture()]})
-        pyramid = psm.Pyramid((-1.5, 0, 0), sides=[
-            {"textures": [psm.HatchTexture(3.5)]},
-            {},
-            {},
-            {"textures": [psm.FillTexture()]}
-        ])
+        cube = psm.Box(
+            (0, 0, 0.5),
+            left={"textures": [psm.HatchTexture(3.5)]},
+            right={"textures": [psm.FillTexture()]},
+        )
+        pyramid = psm.Pyramid(
+            (-1.5, 0, 0),
+            sides=[
+                {"textures": [psm.HatchTexture(3.5)]},
+                {},
+                {},
+                {"textures": [psm.FillTexture()]},
+            ],
+        )
         prism = psm.Prism(
             (1.5, 0, 0.5),
             6,
@@ -27,8 +34,8 @@ class Example03Sketch(vsketch.SketchClass):
                 {},
                 {"textures": [psm.HatchTexture(3.5)]},
                 {"textures": [psm.FillTexture()]},
-                {}
-            ]
+                {},
+            ],
         )
         scene = psm.Scene(frame, self.unit_size, [pyramid, cube, prism])
         scene.render(vsk)
@@ -38,4 +45,4 @@ class Example03Sketch(vsketch.SketchClass):
 
 
 if __name__ == "__main__":
-    Example03Sketch.display()
+    Example02Sketch.display()
