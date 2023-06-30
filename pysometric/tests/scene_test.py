795334e75f041a2cab076fff909698b98981c87b
Sean Voisen <svoisen@adobe.com>
2023-06-30 15:46:59 -0700

    Add unit test for Scene

--- pysometric/tests/scene_test.py
+++ pysometric/tests/scene_test.py
@@ -1,8 +1,7 @@
 import pytest
 import shapely
 
-from ..scene import Scene
-from ..shape import Rectangle
+from ..scene import Scene, RenderableGeometry
 from ..volume import Box
 
 @pytest.fixture
@@ -10,7 +9,21 @@ def frame():
     return shapely.box(0, 0, 500, 500)
 
 class TestScene:
-    def test_add(self, frame):
-        b = Box((0, 0, 0))
-        s = Scene(frame, 100, [b])
-        assert len(s.children) == 1
+    def test_init(self, frame):
+        """Test Scene initialization"""
+        box = Box((0, 0, 0))
+        grid_pitch = 1
+        children = [box]
+        scene = Scene(frame, grid_pitch, children)
+
+        assert scene.render_context.frame == frame
+        assert scene.render_context.grid_pitch == grid_pitch
+        assert scene.children == children
+
+    def test_compile(self, frame): 
+        """Test Scene.compile()""" 
+        box = Box((0, 0, 0)) 
+        scene = Scene(frame, 1, [box])
+        result = scene.compile()
+        assert len(result) == 3 # 3 faces
+        assert all([isinstance(r, RenderableGeometry) for r in result])
